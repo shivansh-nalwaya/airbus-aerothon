db58ce89efad5c10627a2c68c451c4afb5fc9c84
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2019-03-07 15:57:07 +0530

    Updates

---
+++ precache-manifest.50ba435c56e7503d38c91887f105d0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30f173cb6ff0ebc0de1dd4c5f963d9b1",
+    "url": "/static/media/background.30f173cb.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a6da8b397af406a8b017",
+    "url": "/static/js/main.a6da8b39.chunk.js"
+  },
+  {
+    "revision": "d6677e1d893a60d4638b",
+    "url": "/static/js/2.d6677e1d.chunk.js"
+  },
+  {
+    "revision": "d6677e1d893a60d4638b",
+    "url": "/static/css/2.76e89278.chunk.css"
+  },
+  {
+    "revision": "4411b5bd0ffd7bb030420fd29a546e9f",
+    "url": "/index.html"
+  }
+];
